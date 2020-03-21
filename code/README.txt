e3fd379089a42e5b7febd17afc8ed639ce3f033a
evankuo1 <evankuo@gmail.com>
2020-03-21 15:36:41 -0700

    added example program

--- code/README.txt
+++ code/README.txt
@@ -1,1 +1,3 @@
 Authors: Hanwei Li and Evan Kuo
+
+The easiest way to run the program is to run it in Windows.
---
+++ code/helloworld.dart
@@ -0,0 +1,10 @@
+void main() {
+
+	// Hello World
+	print('Hello, World!');
+
+	// Shows Dart's string interpolation
+	var name = "Evan and Hanwei";
+	var className = "CSC 372";
+	print("This is ${name} working on the project for ${className}");
+}
